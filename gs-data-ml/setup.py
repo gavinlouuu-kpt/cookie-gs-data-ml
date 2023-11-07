a5f988e047e0b6898d3d9127b62241161b966a56
gavinlouuu <gavinlou@kingsphase.page>
2023-11-07 15:07:11 +0800

    main gs data ml prep

---
+++ gs-data-ml/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='gas sensor data preparation and feature extraction for machine learning',
+    author='gavinlou',
+    license='MIT',
+)
